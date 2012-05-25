63ce50aa64772f846a05e378dfed38098da53901
Xiaodong <Xiaodong Zhang@XZhang-PC.(none)>
2012-05-25 11:33:19 +0800

    Q2.5

--- src/chapter2/Question2_5.java
+++ src/chapter2/Question2_5.java
@@ -12,4 +12,31 @@ package chapter2;
  */
 public class Question2_5 {
 
+	/* a very tricky solution */
+	public static Node findLoop(Node head) {
+		Node fastRunner = head.next.next;
+		Node slowRunner = head.next;
+		
+		while(fastRunner != slowRunner && fastRunner != null && slowRunner != null) {
+			fastRunner = fastRunner.next.next;
+			slowRunner = slowRunner.next;
+		}
+		slowRunner = head;
+		while(slowRunner != fastRunner) {
+			slowRunner = slowRunner.next;
+			fastRunner = fastRunner.next;
+		}
+		return fastRunner;
+	}
+	
+	public static void main(String[] args) {
+		/* 1 -> 2 -> 3 -> 4 ->5 -> 3 */
+		Node n1 = new Node(new int[] {1, 2, 3, 4, 5});
+		Node n3 = n1.next.next;
+		Node n5 = n3.next.next;
+		n5.next = n3;
+		//n1.print();
+		Node loopStart = findLoop(n1);
+		System.out.print(loopStart.data);
+	}
 }
