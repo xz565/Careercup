b31fe075bf43379cd06333237ce501f3f4a66bbb
Xiaodong Zhang <zhang6078349@163.com>
2012-06-16 07:38:38 -0400

    Q3.6

--- src/chapter3/Question3_6.java
+++ src/chapter3/Question3_6.java
@@ -12,10 +12,65 @@ import java.util.Stack;
  */
 public class Question3_6 {
 
-	public static void sortStack(Stack<Integer> stack) {
-		Stack<Integer> s1 = new Stack<Integer>();
-		Stack<Integer> s2 = new Stack<Integer>();
-		
-		
+	/*
+	 * a brute force method
+	 * time: O(n^2) space: 3n
+	 */
+	public static Stack<Integer> sortStack(Stack<Integer> stack) {
+		Stack<Integer> out = new Stack<Integer>();
+		Stack<Integer> buffer = new Stack<Integer>();
+		while(!stack.isEmpty()) {
+			int min = Integer.MAX_VALUE;
+			while(!stack.isEmpty()) {
+				int temp = stack.pop();
+				min = Math.min(min, temp);
+				buffer.push(temp);
+			}
+			while(!buffer.isEmpty()){
+				int temp = buffer.pop();
+				if(temp == min) {
+					out.push(temp);
+				} else {
+					stack.push(temp);
+				}
+			}
+		}
+		return out;
+	}
+	
+	/*
+	 * a slightly better one
+	 * time: O(n^2) space: 2n
+	 */
+	public static Stack<Integer> sortStack2(Stack<Integer> stack) {
+		Stack<Integer> out = new Stack<Integer>();
+		out.push(stack.pop());
+		while(!stack.isEmpty()) {
+			int temp = stack.pop();
+			int temp2 = out.peek();
+			while(temp > temp2 && !out.isEmpty()) {
+				temp2 = out.pop();
+				stack.push(temp2);
+				if(!out.isEmpty()) {
+					temp2 = out.peek();
+				}
+			}
+			out.push(temp);
+		}
+		return out;
+	}
+	
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(7);
+		stack.push(2);
+		stack.push(4);
+		stack.push(8);
+		stack.push(3);
+		stack.push(10);
+		stack.push(3);
+		System.out.println(stack);
+		Stack<Integer> out = sortStack2(stack);
+		System.out.println(out);
 	}
 }
