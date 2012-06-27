1b5120454638ae7bca090226509d703de974ce61
Xiaodong Zhang <zhang6078349@163.com>
2012-06-26 22:33:08 -0400

    Q4.3 Q4.4 Q4.5

--- src/chapter4/TreeNode.java
+++ src/chapter4/TreeNode.java
@@ -4,27 +4,33 @@ public class TreeNode {
 	int value;
 	TreeNode left;
 	TreeNode right;
+	TreeNode parent;
 	
 	public TreeNode(int val) {
 		value = val;
 		left = null;
 		right = null;
+		parent = null;
 	}
 	
 	public void addLeft(int val) {
-		left = new TreeNode(val);
+		addLeft(new TreeNode(val));
 	}
 	
 	public void addLeft(TreeNode n) {
-		left = n;
+		if(n == null) return;
+		this.left = n;
+		n.parent = this;
 	}
 	
 	public void addRight(int val) {
-		right = new TreeNode(val);
+		addRight(new TreeNode(val));
 	}
 	
 	public void addRight(TreeNode n) {
-		right = n;
+		if(n == null) return;
+		this.right = n;
+		n.parent = this;
 	}
 	public String toString() {
 		return value + "";
