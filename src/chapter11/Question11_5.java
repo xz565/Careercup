d80a011c4453479652c70795a1ac3b7509ad01f3
= <zhang6078349@163.com>
2012-07-13 20:52:09 -0400

    Q11.5 Q11.6

---
+++ src/chapter11/Question11_5.java
@@ -0,0 +1,47 @@
+package chapter11;
+
+/*
+ * Given a sorted array of strings which is 
+ * interspersed with empty strings, 
+ * write a method to find the location of a given string.
+ */
+public class Question11_5 {
+
+	static int findStr(String[] strs, String str, int left, int right) {
+		if(left > right) return -1;
+		
+		int middle = (left + right) / 2;
+		if(strs[middle].equals("")) {
+			int mLeft = middle - 1;
+			int mRight = middle + 1;
+			while(true) {
+				if(mLeft < left || mRight > right)
+					return -1;
+				
+				if(!strs[mLeft].equals("")) {
+					middle = mLeft;
+					break;
+				}
+				if(!strs[mRight].equals("")) {
+					middle = mRight;
+					break;
+				}
+				mLeft++;
+				mRight++;
+			}
+		}
+		
+		if(strs[middle].compareTo(str) > 0)
+			return findStr(strs, str, left, middle - 1);
+		else if(strs[middle].compareTo(str) == 0)
+			return middle;
+		else return findStr(strs, str, middle + 1, right);
+	}
+	
+	public static void main(String[] args) {
+		String[] strs = {"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""};
+		System.out.println(findStr(strs, "at", 0, strs.length - 1));
+		System.out.println(findStr(strs, "dad", 0, strs.length - 1));
+		System.out.println(findStr(strs, "ballcar", 0, strs.length - 1));
+	}
+}
---
+++ src/chapter11/Question11_6.java
@@ -0,0 +1,34 @@
+package chapter11;
+
+/*
+ * Given a matrix in which each row and each column is sorted, 
+ * write a method to find an element in it.
+ */
+public class Question11_6 {
+
+	static boolean searchM(int[][] matrix, int num) {
+		int row = 0;
+		int column = matrix[0].length - 1;
+		
+		while(row < matrix.length && column >= 0) {
+			if(matrix[row][column] == num)
+				return true;
+			else if(matrix[row][column] > num) {
+				column--;
+			} else {
+				row++;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		int[][] matrix = {{15, 20, 40,  85}, 
+				{20, 35, 80, 95}, 
+				{30, 55, 95, 105},
+				{40, 80, 100, 120}};
+		
+		System.out.println(searchM(matrix, 100));
+		System.out.println(searchM(matrix, 60));
+	}
+}
