8af9081d50782378d3c61bc8a1ab01d6be97456f
xiaodong <zhang6078349@163.com>
2012-09-30 01:30:42 -0400

    Simple Singleton Design Pattern

---
+++ src/Interview/MySingleton.java
@@ -0,0 +1,15 @@
+package Interview;
+
+public class MySingleton {
+
+	private static MySingleton instance;
+
+	private MySingleton() {}
+	
+	public static synchronized MySingleton getInstance() {
+		if(instance == null)
+			instance = new MySingleton();
+		
+		return instance;
+	}
+}
--- src/Interview/Test.java
+++ src/Interview/Test.java
@@ -10,5 +10,13 @@ public class Test {
 		strs.add("2");
 		strs.add("3");
 		System.out.println(strs);
+		
+		double d1 = 0.4;
+		double d3 = 0.2;
+		double d2 = 0.8;
+		double d4 = 0.4;
+		System.out.println(Math.abs(d1 / d3));
+		System.out.println(Math.abs(d2 / d4));
+		System.out.println("d1 == d2 ? " + (Math.abs(d1 / d3) == Math.abs(d2 / d4)));
 	}
 }
