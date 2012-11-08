10fda179050f7871bddd34d6b2130f989d73f850
xiaodong <zhang6078349@163.com>
2012-11-08 15:22:53 -0500

    OOD for Deck of Card

---
+++ src/chapter8/Hand.java
@@ -0,0 +1,19 @@
+package chapter8;
+
+import java.util.ArrayList;
+
+public class Hand <T extends Card> {
+	protected ArrayList<T> cards = new ArrayList<T>();
+	
+	public int score() {
+		int score = 0;
+		for(T card : cards) {
+			score += card.value();
+		}
+		return score;
+	}
+	
+	public void addCard(T card) {
+		cards.add(card);
+	}
+}
