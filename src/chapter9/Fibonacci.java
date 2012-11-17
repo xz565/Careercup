d10c2263f6ba97bfe6600edabd2f1a70e0ea62dc
xiaodong <zhang6078349@163.com>
2012-11-17 11:06:46 -0500

    Add an iterative solution to Fibonacci

--- src/chapter9/Fibonacci.java
+++ src/chapter9/Fibonacci.java
@@ -21,10 +21,33 @@ public class Fibonacci {
 		return cache[n];
 	}
 	
+	public static long Fib2(int n) {
+		if(n < 0) return -1;
+		if(n == 0) return 0;
+		if(n == 1) return 1;
+		
+		int tmp1 = 0;
+		int tmp2 = 1;
+		int currt = tmp1 + tmp2;
+		
+		for(int i = 3; i <= n; i++) {
+			tmp1 = tmp2;
+			tmp2 = currt;
+			currt = tmp1 + tmp2;
+		}
+		
+		return currt;
+	}
+	
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		System.out.println(Fib(45));
 		long end = System.currentTimeMillis();
+		System.out.println("Time: " + (end - start));
+		
+		start = System.currentTimeMillis();
+		System.out.println(Fib2(45));
+		end = System.currentTimeMillis();
 		System.out.println("Time: " + (end - start));
 		
 		start = System.currentTimeMillis();
