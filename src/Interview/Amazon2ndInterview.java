71e37232fab874b6f819bef13a0af6ef549a174c
Xiaodong Zhang <xz565@nyu.edu>
2013-01-06 12:09:13 +0800

    added a logn solution

--- src/Interview/Amazon2ndInterview.java
+++ src/Interview/Amazon2ndInterview.java
@@ -43,12 +43,72 @@ public class Amazon2ndInterview {
 		return -1;
 	}
 	
+	
+	// A recursive solution utilizing binary search technology
+	// It takes O(lgn) time
+	public static int findMissing2(int[] arr, int n) {
+		if(arr == null)
+			return -1;
+		
+		if(arr.length != n)
+			return -1;
+		
+		if(arr[0] == 0 && arr[n-1] == n-1)
+			return n;
+		
+		if(arr[0] == 1 && arr[n-1] == n)
+			return 0;
+		
+		int head = 0;
+		int tail = n - 1;
+		
+		while(head < tail) {
+			if(tail - head == 1)
+				return tail;
+			
+			int mid = (head + tail) / 2; // overflow
+			
+			int arrHeadDiff = arr[mid] - arr[head];
+			int idxHeadDiff = mid - head;
+			
+			int arrTailDiff = arr[tail] - arr[mid];
+			int idxTailDiff = tail - mid;
+			
+			if(arrHeadDiff != idxHeadDiff) {
+				tail = mid;
+			}
+			
+			if(arrTailDiff != idxTailDiff) {
+				head = mid;
+			}
+		}
+		
+		return -1;
+	}
+	
 	public static void main(String[] args) {
 		int[] arr = {0, 1, 2, 3, 4};
 		System.out.println(findMissing(arr, 5));
+		System.out.println(findMissing2(arr, 5));
+		
 		int[] arr2 = {1, 2, 3, 4, 5};
 		System.out.println(findMissing(arr2, 5));
+		System.out.println(findMissing2(arr2, 5));
+		
 		int[] arr3 = {0, 1, 2, 4, 5};
 		System.out.println(findMissing(arr3, 5));
+		System.out.println(findMissing2(arr3, 5));
+		
+		int[] arr4 = {0, 1, 2, 4, 5, 6, 7, 8, 9};
+		System.out.println(findMissing(arr4, arr4.length));
+		System.out.println(findMissing2(arr4, arr4.length));
+		
+		int[] arr5 = {0, 1, 2, 4, 5, 6, 7, 8, 9, 10};
+		System.out.println(findMissing(arr5, arr5.length));
+		System.out.println(findMissing2(arr5, arr5.length));
+		
+		int[] arr6 = {0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11};
+		System.out.println(findMissing(arr6, arr6.length));
+		System.out.println(findMissing2(arr6, arr6.length));
 	}
 }
