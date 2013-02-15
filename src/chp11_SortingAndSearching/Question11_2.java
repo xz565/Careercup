755bd79e8d377bb8ce554cdf32a511bcaa9d419a
xiaodong <zhang6078349@163.com>
2013-02-15 13:08:25 -0500

    added an solution to Q11.2

--- src/chp11_SortingAndSearching/Question11_2.java
+++ src/chp11_SortingAndSearching/Question11_2.java
@@ -2,11 +2,15 @@ package chp11_SortingAndSearching;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 /*
  * Write a method to sort an array of strings so that 
  * all the anagrams are next to each other.
  */
+
+// This is a O(nlgn) solution for this Problem
 public class Question11_2 implements Comparator<String>{
 	static String sortStr(String str) {
 		char[] chars = str.toCharArray();
@@ -19,12 +23,43 @@ public class Question11_2 implements Comparator<String>{
 		return sortStr(o1).compareTo(sortStr(o2));
 	}
 	
+	// a faster solution using hashing
+	public static void sort(String[] array) {
+		HashMap<String, LinkedList<String>> hash = 
+				new HashMap<String, LinkedList<String>>();
+		
+		for(String str : array) {
+			String keyStr = sortStr(str);
+			if(hash.containsKey(keyStr)) {
+				hash.get(keyStr).add(str);
+			} else {
+				LinkedList<String> list = new LinkedList<String>();
+				list.add(str);
+				hash.put(keyStr, list);
+			}
+		}
+		
+		int idx = 0;
+		for(String key : hash.keySet()) {
+			for(String str : hash.get(key)) {
+				array[idx] = str;
+				idx++;
+			}
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		String[] array = {"asdfg", "zxcvb", "qwert", "gfdsa", "trewq", "vcbxz"};
 		Arrays.sort(array, new Question11_2());
-		for(String s : array) {
+		for(String s : array)
 			System.out.println(s);
-		}
+		System.out.println();
+		
+		String[] array2 = {"asdfg", "zxcvb", "qwert", "gfdsa", "trewq", "vcbxz"};
+		sort(array2);
+		for(String s : array)
+			System.out.println(s);
 	}
 
 }
